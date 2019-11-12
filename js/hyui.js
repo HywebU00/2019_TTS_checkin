70f79e678704d14bb8b7cb8a586faf6c6deb1438
hahamimidido <eason@tpe.hyweb.com.tw>
2019-11-12 11:55:52 +0800

    update 查詢結果tab

--- js/hyui.js
+++ js/hyui.js
@@ -375,9 +375,8 @@ $(function() {
         resizeTimer1 = setTimeout(function() {
             ww = _window.outerWidth();
             tabSet();
-        }, 50);
+        }, 0);
     });
-
     function tabSet() {
         $('.tabs').each(function() {
             var _tab = $(this),
@@ -391,32 +390,27 @@ $(function() {
                 tabItemLength = _tabItem.length,
                 tabItemWidth;
             _tab.find('.active').next('.tabContent').show();
-            if (ww >= wwSmall) {
+
                 _tabContent.css('top', tabItemHeight);
                 _tab.height(tabContentHeight + tabItemHeight);
                 tabItemWidth = (tabwidth - (tabItemLength - 1) * tiGap) / tabItemLength;
                 _tabItem.width(tabItemWidth).css('margin-left', tiGap);
                 _tabItem.first().css('margin-left', 0);
                 _tabItem.last().css({ 'position': 'absolute', 'top': 0, 'right': 0 }).width(tabItemWidth);
-            } else {
-                _tab.css('height', 'auto');
-                _tabItem.width(tabwidth);
-                _tabItem.css('margin-left', 0).last().css('position', 'relative');
-            }
             _tabItemA.focus(tabs);
             _tabItemA.click(tabs);
 
             function tabs(e) {
                 var _tabItemNow = $(this).parent(),
                     tvp = _tab.offset().top,
-                    tabIndex = _tabItemNow.index() / 2,
-                    scollDistance = tvp + tabItemHeight * tabIndex - hh;
+                    tabIndex = _tabItemNow.index() / 2;
+                    // scollDistance = tvp + tabItemHeight * tabIndex - hh;
                 _tabItem.removeClass('active');
                 _tabItemNow.addClass('active');
                 if (ww <= wwSmall) {
                     _tabItem.not('.active').next().slideUp();
                     _tabItemNow.next().slideDown();
-                    $("html,body").stop(true, false).animate({ scrollTop: scollDistance });
+                    // $("html,body").stop(true, false).animate({ scrollTop: scollDistance });
                 } else {
                     _tabItem.not('.active').next().hide();
                     _tabItemNow.next().show();
